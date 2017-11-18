ff464d1a936755a6dee847c6d7cedfe99b66f828
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-11-18 11:28:24 -0500

    Add files via upload

---
+++ PracticePython_Exc1.py
@@ -0,0 +1,11 @@
+name = raw_input("Enter your name: ")
+print("Your name is " + name)
+
+age = raw_input("Enter your age: ")
+print("Your age is " + age)
+
+old_year = 100 - int(age)
+total = 0
+total = total + old_year + 2017
+
+print("The year that you will turn 100 years old is " + str(total))
