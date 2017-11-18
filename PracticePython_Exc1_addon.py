4a4549ae112bfdb93f3bca8de0a3130d9cd134b3
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-11-18 11:33:02 -0500

    Add files via upload
    This file uses \n command to print without having to press enter.

---
+++ PracticePython_Exc1_addon.py
@@ -0,0 +1,11 @@
+name = raw_input("Enter your name: ")
+print("\nYour name is " + name)
+
+age = raw_input("Enter your age: ")
+print("\nYour age is " + age)
+
+old_year = 100 - int(age)
+total = 0
+total = total + old_year + 2017
+
+print("\nThe year that you will turn 100 years old is " + str(total))
